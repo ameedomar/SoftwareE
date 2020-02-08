374c865e88ca4e3762775412b43dc025c2f9638e
DELL <DELL@DESKTOP-MBL3DJ8.home>
2020-02-08 18:42:01 +0200

    Editing on code

--- src/printer.java
+++ src/printer.java
@@ -1,9 +1,15 @@
 
 public class printer {
-
+ public int function1(int r) {
+	return 2*r; 
+ }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("AMEED Software Enginnering");
+		printer p =  new printer();
+		int output= p.function1(4);
+		System.out.printf("AMEED Software Enginnering\n");
+		System.out.print(""+output);
 
 	}
 
